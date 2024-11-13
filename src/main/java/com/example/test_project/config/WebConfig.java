0fa655893c8a4cf1f256fbef0dac97b05970691b
b19301 <bnine19301@gmail.com>
2024-11-13 17:18:12 +0900

    CORS 수정

--- src/main/java/com/example/test_project/config/WebConfig.java
+++ src/main/java/com/example/test_project/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.test_project.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
