8aa254d64b24a2232cedd2489ba95b3ee1f8dc1f
b19301 <bnine19301@gmail.com>
2024-11-14 13:29:59 +0900

    yml address 수정

--- src/main/java/com/example/test_project/controller/TestController.java
+++ src/main/java/com/example/test_project/controller/TestController.java
@@ -3,7 +3,6 @@ package com.example.test_project.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
